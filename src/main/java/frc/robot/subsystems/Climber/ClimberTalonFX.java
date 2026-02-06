c5a94d59a4c012ed644e26ef01bafd591963238c
VioletY <123700864+VioletYarKhan@users.noreply.github.com>
2026-02-06 10:57:14 -0500

    Climber IO added

---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberTalonFX.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.Climber;
+
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
+import frc.robot.Constants.ShooterConstants;
+
+public class ClimberTalonFX extends SubsystemBase {
+  
+}
